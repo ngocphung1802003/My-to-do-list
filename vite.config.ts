e0455a2b4fa7d14bc00362105e9678a30952f109
Nguyễn Phụng <nguyenphung@192.168.1.243>
2026-03-29 16:13:10 +0700

    Fix: remove base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/My-to-do-list/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
